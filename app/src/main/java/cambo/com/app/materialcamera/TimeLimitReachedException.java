5deee6f6577655f8a4efc447701dff40e9dd8d39
Bonvoath <bonvoathtanson@gmail.com>
2018-10-19 15:54:31 +0700

    upgrade version sdk

---
+++ app/src/main/java/cambo/com/app/materialcamera/TimeLimitReachedException.java
@@ -0,0 +1,9 @@
+package cambo.com.app.materialcamera;
+
+/** @author Aidan Follestad (afollestad) */
+public class TimeLimitReachedException extends Exception {
+
+  public TimeLimitReachedException() {
+    super("You've reached the time limit without starting a recording.");
+  }
+}
